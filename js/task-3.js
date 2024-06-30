bf41f87581685dcc2cdba2e091ff11f82314e710
ihor-voznyi <haiku.kiev@gmail.com>
2024-06-30 19:09:03 +0300

    upd task3

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const profile = {
+    username: 'Jacob',
+    payTime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        `${this.username} has ${this.payTime} active hours!`
+    }
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
