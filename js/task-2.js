413abe58c2e2be1e18cf23e722251cc9078a2ea3
ihor-voznyi <haiku.kiev@gmail.com>
2024-07-04 22:01:01 +0300

    upd task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function calcAverageCalories(days) {
-    let totalCalories = 0;
+    let averageCalories = 0;
 
     if (days.length === 0) return 0;
     for (const day of days) {
